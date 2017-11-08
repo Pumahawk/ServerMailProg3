fe0e8007c913fd2396b01621491ba8175b9d4b58
Lorenzo Gandino <lorenzoga94@gmail.com>
2017-11-08 16:03:00 +0100

    Inizializzato client

--- src/client/ClientApp.java
+++ src/client/ClientApp.java
@@ -1,5 +1,44 @@
 package client;
 
-public class ClientApp {
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
+import mail.CasellaElettronicaBase;
+import mail.ServerMailBase;
+
+public class ClientApp {
+	
+	String mail;
+	CasellaElettronicaBase caselleElettronica;
+	
+	
+	public ClientApp(String mail) {
+		this.mail = mail;
+	}
+	
+	public static void main(String[] args) {
+		ServerMailBase server;
+		ClientApp app = new ClientApp("lorenzo@gmail.com");
+		try {
+			server = (ServerMailBase) Naming.lookup("//localhost/AppServer");
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			System.err.println("Errore collegamento al server.");
+			System.err.println("Controllare che il server sia raggiungibile");
+			e.printStackTrace();
+			server = null;
+			System.exit(-1);
+		}
+		try {
+			app.caselleElettronica = server.loginMail(app.mail);
+		} catch (RemoteException e) {
+			System.err.println("Errore login server.");
+			e.printStackTrace();
+			server = null;
+			System.exit(-1);
+		}
+		
+		
+	}
 }
