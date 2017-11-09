b1f3ecc4fa2d59f974bfee897893d0db8217348b
Lorenzo Gandino <lorenzoga94@gmail.com>
2017-11-09 15:33:24 +0100

    Collegato il Logger all'interfaccia grafica.

--- src/server/ServerLogger.java
+++ src/server/ServerLogger.java
@@ -1,33 +1,46 @@
 package server;
 
+import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 
 import mail.ServerEvent;
 import mail.ServerListener;
 
-public class ServerLogger implements ServerListener{
-
+public class ServerLogger implements ServerListener {
+	
 	Document lg;
+
+	
 	public ServerLogger(Document lg) {
 		this.lg = lg;
 	}
+
+	void println(String s) {
+		try {
+			this.lg.insertString(lg.getEndPosition().getOffset()-1, s + '\n', null);
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
 	@Override
-	public void login(ServerEvent e, String mail) {
-		System.out.println("<ServerController:login>");
-		System.out.println("Richiesta login mail: " + mail);
+	public synchronized void login(ServerEvent e, String mail) {
+		println("<ServerController:login>");
+		println("Richiesta login mail: " + mail);
 	}
 
 	@Override
-	public void mailExist(ServerEvent e, String mail) {
-		System.out.println("<ServerController:logimailExist>");
-		System.out.println("Richiesta verifica esistenza mail: " + mail);
+	public synchronized void mailExist(ServerEvent e, String mail) {
+		println("<ServerController:logimailExist>");
+		println("Richiesta verifica esistenza mail: " + mail);
 		
 	}
 
 	@Override
-	public void creazioneMail(ServerEvent e, String mail) {
-		System.out.println("<ServerController:creazioneMail>");
-		System.out.println("Richiesta creazione mail: " + mail);
+	public synchronized void creazioneMail(ServerEvent e, String mail) {
+		println("<ServerController:creazioneMail>");
+		println("Richiesta creazione mail: " + mail);
 		
 	}
 
