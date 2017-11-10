a5872a806cd1fb1a5960870ffbe5b99f982c2f85
Lorenzo Gandino <lorenzoga94@gmail.com>
2017-11-10 11:24:28 +0100

    Aggiunti dati Mail in MailEvent, aggiornato ServerLog
    Ora ServerLog controlla anche le caselle elettroniche

--- src/mail/Mail.java
+++ src/mail/Mail.java
@@ -6,10 +6,14 @@ public class Mail implements Serializable{
 	
 	private static final long serialVersionUID = 6350421221845772435L;
 	
+	public final String mittente;
+	public final String[] destinatari;
 	public final String oggetto;
 	public final String testo;
 	
-	public Mail(String oggetto, String testo) {
+	public Mail(String mittente, String[] destinatari, String oggetto, String testo) {
+		this.mittente = mittente;
+		this.destinatari = destinatari;
 		this.oggetto = oggetto;
 		this.testo = testo;
 	}
--- src/server/ServerLogger.java
+++ src/server/ServerLogger.java
@@ -3,41 +3,64 @@ package server;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 
-public class ServerLogger implements ServerListener {
+import mail.MailEvent;
+import mail.MailListener;
+
+public class ServerLogger implements ServerListener, MailListener {
 	
 	Document lg;
+	private final String nameClass;
 
 	
 	public ServerLogger(Document lg) {
 		this.lg = lg;
+		this.nameClass = getClass().getName();
 	}
 
 	void println(String s) {
 		try {
 			this.lg.insertString(lg.getEndPosition().getOffset()-1, s + '\n', null);
 		} catch (BadLocationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(-1);
 		}
 	}
 	@Override
 	public synchronized void login(ServerEvent e, String mail) {
-		println("<ServerController:login>");
+		println("<"+nameClass+":login>");
 		println("Richiesta login mail: " + mail);
 	}
 
 	@Override
 	public synchronized void mailExist(ServerEvent e, String mail) {
-		println("<ServerController:logimailExist>");
+		println("<"+nameClass+":logimailExist>");
 		println("Richiesta verifica esistenza mail: " + mail);
 		
 	}
 
 	@Override
 	public synchronized void creazioneMail(ServerEvent e, String mail) {
-		println("<ServerController:creazioneMail>");
+		println("<"+nameClass+":creazioneMail>");
 		println("Richiesta creazione mail: " + mail);
+		
+	}
+
+	@Override
+	public synchronized void mailInviata(MailEvent e) {
+		println("<"+nameClass+":mailInviata>");
+		println("Mittente: " + e.mail.mittente);
+		println("Destinatari:");
+		for(String destinatario : e.mail.destinatari)
+			println("--> " + destinatario);
+	}
+
+	@Override
+	public synchronized void mailRicevuta(MailEvent e) {
+		println("<"+nameClass+":mailInviata>");
+		println("Mittente: " + e.mail.mittente);
+		println("Destinatari:");
+		for(String destinatario : e.mail.destinatari)
+			println("--> " + destinatario);
 		
 	}
 
